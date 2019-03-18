d50c01c0f3a6a5ffb1910a0e1f124b8cc161c33d
Victorino <victorinoeng@gmail.com>
2019-03-18 10:45:19 +0000

    fix: building from /site

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "A/B Testing Summary",
+        "excerpt":"Testing MarkDown for article writing.   Adding an image example:    import pandas as pd import numpy as np  Inline code: np.random.normal()   Math example:    Header2   Header 3   Header 4   ","categories": [],
+        "tags": ["Data Analysis"],
+        "url": "http://localhost:4000/site/ABtesting/",
+        "teaser":null}]
