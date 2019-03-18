62b13b002b615763b6d43f144369bbe3930021c7
Victorino <victorinoeng@gmail.com>
2019-03-18 11:15:32 +0000

    add: admin page for Content Management

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "A/B Testing Summary",
         "excerpt":"Testing MarkDown for article writing.   Adding an image example:    import pandas as pd import numpy as np  Inline code: np.random.normal()   Math example:    Header2   Header 3   Header 4   ","categories": [],
         "tags": ["Data Analysis"],
-        "url": "http://localhost:4000/_site/ABtesting/",
+        "url": "http://localhost:4000/ABtesting/",
         "teaser":null}]
